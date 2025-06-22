e997e90f214040f019051d94cca83e722adac606
tlaloc69 <142059205+framirez64@users.noreply.github.com>
2025-06-22 14:58:16 -0500

    Finished mayve

--- Franp/src/components/Projects.tsx
+++ Franp/src/components/Projects.tsx
@@ -25,14 +25,14 @@ function Projects() {
       id: 'first',
       title: 'Eidolon - Webapp',
       heading: 'AI Personal Assistant',
-      content: 'stuff',
+      content: "The Eidolon WebApplication is my the latest project I've developed and the one I'm currently iterating on. Eidolon is meant to be an AI assistant powered by Google's Gemini AI, it has decentralized aaccount creation (no email or phone number tied to your account), and you just need your generated private key to login. Eidolon is meant to provide AI generated suggestions based on your goals, interests, and hobbies by generating daily plans, meals, and helping you track your sleep and emotions. The two componennts I've recently added are a personal library tied to a user, along with note-taking capabilities(currently under development). The library allows users to upload and store material they're currently reading, and view it from anywhere. The goal is to have a personal AI assistant that mirrors your thinking and can assist you in different tasks. I used React + Vite for developing the front-end and styling. For the back-end I utilized Firebase for storage, requests, API integration, and functions. The decentralized identity was created using the NOSTR protocol. I utilized GitHub for version control and for developing the UIs I used Figma.",
       slides: [Assistant1, Assistant2, Assistant3, Assistant4],
     },
     {
       id: 'second',
       title: 'Eidolon - Desktop App',
       heading: 'Local AI Inference Engine',
-      content: 'stuff',
+      content: 'The Eidolon Desktop App is an adjacent project I created that in the future will work in tandem with the Eidolon Webapp. A core component of the webapplication was the decentralized identity for users. To expand on that, I wanted to make something that would take that a step further by allowing users to not rely on using AI ran by other companies. For that, I came up with the of the Eidolon desktop app which allows a user to run a local AI on their computer for interactions to avoid having their interactions tracked by companies. Some industries such as law and clinical would benefit greatly from utilziing AI in their workflows but legally cannot disclose sensitive information. This version of the Eidolon project is a desktop application for Windows that runs in two modes: inference and server. In the inference-mode, the application launches a window for users to interact with an AI with. One cool feature of this AI is that it contains a "thinking" mode which can be toggled on or off. In thinking mode, the AI ponders its answers more to showcase the steps it took to come up with an answer. If a user simply wants a straight-forward answer, thinking-mode can be turned off. When fully developed and paired up, the Eidolon desktop application could be ran in server-mode to connect with the Eidolon webpage so that any questions and information sent to the AI is fully local and private. I used the Ollama open-source project as the foundation for this project and utilized QT6 for developing the UI.',
       slides: [Eidolon1, Eidolon2],
     },
     {
